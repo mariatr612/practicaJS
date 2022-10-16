3896722efac9ce05b24dd3278accc81b7d45b245
Bustos Danilo <danilo.bustos.702@gmail.com>
2022-10-16 11:58:02 -0300

    practicaJS

---
+++ practica.js
@@ -0,0 +1,15 @@
+
+
+
+
+    var calle = prompt("Dime tu calle");
+    var casa = prompt("Dime tu numero de casa");
+    var codigoPostal = prompt("Dime tu codigo postal");
+    var ciudad = prompt("Dime tu ciudad");
+    var pais = prompt("Dime tu pais");
+    
+    document.write("La dirección ingresada es: "+calle+"   "+casa+" , "+codigoPostal+" , " +ciudad+ " , " +pais+ ".");
+
+
+    
+    
